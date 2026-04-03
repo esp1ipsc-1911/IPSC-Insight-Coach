255b196f7a9fd8344a8ede8c11b55ea4652ee6d4
esp1ipsc-1911 <esp1.ipsc@gmail.com>
2026-04-03 18:57:21 +0200

    Update appUI.js

--- src/appUI.js
+++ src/appUI.js
@@ -1,8 +1,9 @@
 export function renderApp(container) {
   container.innerHTML = `
-    <div style="padding:40px;font-family:sans-serif">
+    <div style="padding:40px;font-family:sans-serif;max-width:700px;margin:0 auto">
       <h1>IPSC Insight</h1>
       <p>Du er logget inn ✅</p>
+      <p>Pålogging og registrering er nå koblet til Firebase Authentication og Firestore.</p>
     </div>
   `;
 }
